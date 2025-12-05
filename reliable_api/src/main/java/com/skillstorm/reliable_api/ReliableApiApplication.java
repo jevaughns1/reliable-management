72413f1c614c9a4db9c51cc36a02d6f89e461fb7
jevaughns1 <jevaughnstewart100@gmail.com>
2025-12-05 16:15:18 -0500

    First Commit

---
+++ reliable_api/src/main/java/com/skillstorm/reliable_api/ReliableApiApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.reliable_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReliableApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReliableApiApplication.class, args);
+	}
+
+}
---
+++ reliable_api/src/test/java/com/skillstorm/reliable_api/ReliableApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillstorm.reliable_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReliableApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
